9c410b133450ba86cd9c06c742cc32dfca5cbc92
fukiame <fukiame@proton.me>
2024-11-03 09:13:24 +0700

    wrapper: nuke KernelSU dir manually
    Signed-off-by: fukiame <fukiame@proton.me>

--- wrapper.sh
+++ wrapper.sh
@@ -42,6 +42,7 @@ else
   git checkout ${kernel_branch}
   git fetch origin ${kernel_branch}
   git reset --hard origin/${kernel_branch}
+  rm -rf KernelSU
   git submodule update --init --recursive
 fi
 
