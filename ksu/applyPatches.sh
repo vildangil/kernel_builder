a78228930a4ef4444c7391462afaf66b5706c39a
fukiame <fukiame@proton.me>
2025-01-02 10:02:28 +0700

    extremely sussy behaviors because sus sus amogus sussy baka sigma drip

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -11,14 +11,25 @@ git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
-if [[ -d "$patchesdir" ]]; then
-  for patch_file in "$patchesdir"/*.patch ; do
+short_k_ver="$(echo $kernel_ver | cut -d. -f1,2)"
+
+patchesdir="$outside/ksu/patches"
+kpatchesdir="$patchesdir/$short_k_ver"
+susdir="$outside/ksu/susfs/kernel_patches"
+
+if [[ -d "$kpatchesdir" ]]; then
+  for patch_file in "$kpatchesdir"/*.patch ; do
     git am "$patch_file"
   done
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
+fi
+
+if [[ "$short_k_ver" == "4.19" ]] ; then
+  cp "$susdir/fs/susfs.c" ./fs
+  cp "$susdir/include/linux/susfs.h" ./include/linux
+  cd KernelSU && git am "$patchesdir/KernelSU/*" && cd ..
 fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
