e6f3dcdf0743e86b00511e147e2dc4454e793091
fukiame <fukiame@proton.me>
2025-05-08 06:18:57 +0700

    amogus my special
    seems like it wanna kill others, isolate it for now
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -6,10 +6,22 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/refs/heads/main/kernel/setup.sh" | bash -s susfs-v1.5.7
+git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
+patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
 suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
+
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
 
 if [[ -d "$suspatchesdir" ]]; then
   for patch_file in "$suspatchesdir"/*.patch ; do
@@ -20,10 +32,10 @@ else
   exit 1
 fi
 
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}-sus\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}sus\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
 echo -e " \nincludes rsuntk's KernelSU fork, ver ${KSU_ver}" >> banner_append
-echo -e " \nincludes SusFS v1.5.7, also backported by rsuntk" >> banner_append
+echo -e " \nincludes SuSFS v1.5.7" >> banner_append
 
