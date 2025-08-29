2eeec1e56c05afc732e30c007dc2f5560f7fedff
fukiame <fukiame@proton.me>
2025-08-29 21:34:01 +0700

    [gore] build.sh: always upload build logs
    Signed-off-by: fukiame <fukiame@proton.me>

--- build.sh
+++ build.sh
@@ -51,6 +51,10 @@ for toolchain in $1; do
     echo "build succeeded in $((DIFF / 60))m, $((DIFF % 60))s" > "${zip_name}.info"
     echo "md5: $(md5sum "${zip_name}" | cut -d' ' -f1)" >> "${zip_name}.info"
     echo "compiler: $(cat ${toolchain}.info)" >> "${zip_name}.info"
+
+    echo "build succeeded in $((DIFF / 60))m, $((DIFF % 60))s" > "${toolchain}.log.info"
+    echo "ak3 zip file: ${zip_name}" >> "${toolchain}.log.info"
+    echo "compiler: $(cat ${toolchain}.info)" >> "${toolchain}.log.info"
   else
     BUILD_END=$(date +"%s")
     DIFF=$((BUILD_END - BUILD_START))
