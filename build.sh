4a5e344ec3b78e835631e96605df03c0cf0f8c27
fukiame <fukiame@proton.me>
2025-10-24 02:42:59 +0700

    nice
    Signed-off-by: fukiame <fukiame@proton.me>

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ for toolchain in $1; do
     DIFF=$((BUILD_END - BUILD_START))
     pack ${zip_name}
     echo "build succeeded in $((DIFF / 60))m, $((DIFF % 60))s" > "${zip_name}.info"
-    echo "md5: $(md5sum "${zip_name}" | cut -d' ' -f1)" >> "${zip_name}.info"
+    echo "md5: <code>$(md5sum "${zip_name}</code>" | cut -d' ' -f1)" >> "${zip_name}.info"
     echo "compiler: $(cat ${toolchain}.info)" >> "${zip_name}.info"
 
     echo "build succeeded in $((DIFF / 60))m, $((DIFF % 60))s" > "${toolchain}.log.info"
