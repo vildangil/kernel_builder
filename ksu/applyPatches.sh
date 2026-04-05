8693b8e72680f7a493fc71acd89af0275e5b4a69
VildanG <gildanovvildan0@gmail.com>
2026-04-05 22:59:20 +0300

    Update applyPatches.sh

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -1,51 +1,30 @@
 #!/bin/bash
+#
+# hdjsjfjjwufbeizihfjejzf
+
 export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-# Настройка Git для работы с патчами
-git config --global user.email "bot@example.com"
-git config --global user.name "Kernel Bot"
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/dev/kernel/setup.sh" | bash -s legacy
+git add . && git commit -am "drivers: KernelSU"
+KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 30000))
 
-# Очистка перед началом
-git am --abort >/dev/null 2>&1
-git reset --hard HEAD
-
-echo "--- Установка SukiSU Ultra (Pure) ---"
-# Удаляем старую папку и скачиваем SukiSU Ultra
-rm -rf drivers/kernelsu
-git clone https://github.com/sukisu-ultra/sukisu-ultra drivers/kernelsu
-
-# --- ГАРАНТИЯ СБОРКИ (КРИТИЧНО) ---
-# 1. Прописываем в Makefile
-sed -i '/kernelsu/d' drivers/Makefile
-echo "obj-y += kernelsu/" >> drivers/Makefile
-
-# 2. Убираем все зависимости в Kconfig, чтобы модуль не отключился
-if [ -f "drivers/kernelsu/Kconfig" ]; then
-    sed -i 's/depends on .*//g' drivers/kernelsu/Kconfig
+echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
+patchesdir="$outside/ksu/patches/"
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
 fi
 
-# --- НАСТРОЙКА DEFCONFIG ---
-TARGET_CONFIG="arch/arm64/configs/blossom_defconfig"
-if [ -f "$TARGET_CONFIG" ]; then
-    echo "--- Настройка $TARGET_CONFIG ---"
-    # Удаляем старые записи KSU и SusFS
-    sed -i '/CONFIG_KSU/d' "$TARGET_CONFIG"
-    sed -i '/CONFIG_SUSFS/d' "$TARGET_CONFIG"
-    sed -i '/CONFIG_KPROBES/d' "$TARGET_CONFIG"
-    sed -i '/CONFIG_OVERLAY_FS/d' "$TARGET_CONFIG"
-    
-    # Добавляем только нужные флаги
-    cat <<EOF >> "$TARGET_CONFIG"
-CONFIG_KPROBES=y
-CONFIG_KPROBE_EVENTS=y
-CONFIG_OVERLAY_FS=y
-CONFIG_KSU=y
-CONFIG_LOCALVERSION="-Blackside_SukiSU"
-EOF
-fi
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
 
-# Сохраняем изменения, чтобы GitHub Actions их увидел
-git add .
-git commit -m "Integrate SukiSU Ultra (No SusFS)"
+echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+
+echo -e " \nincludes KernelSU-Next, ver ${KSU_ver}" >> banner_append
+
