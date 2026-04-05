ae85a1a0b537186602e873cda7d832f18f923609
VildanG <gildanovvildan0@gmail.com>
2026-04-05 23:01:26 +0300

    Update asus.sh

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -34,6 +34,6 @@ else
   exit 1
 fi
 
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}sus\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
