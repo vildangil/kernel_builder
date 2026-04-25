63f392f8694249140582bab46f155a58495af552
VildanG <gildanovvildan0@gmail.com>
2026-04-25 22:10:24 +0300

    Update applyPatches.sh

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -23,7 +23,7 @@ else
   exit 1
 fi
 
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-SUSFS\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
