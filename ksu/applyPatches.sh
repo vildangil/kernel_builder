2f9cbff9662f16f35e118ea6853b43cb95c66fa8
VildanG <gildanovvildan0@gmail.com>
2026-05-06 20:55:29 +0300

    Update applyPatches.sh

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -12,6 +12,7 @@ KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
 
 echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
+echo 'CONFIG_KSU_MANUAL_HOOK=y' >> "${defconfig_file}"
 patchesdir="$outside/ksu/patches/"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
