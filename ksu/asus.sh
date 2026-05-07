51040d33a0c6a04c512897ee9fe9b68570e6ff97
VildanG <gildanovvildan0@gmail.com>
2026-05-07 10:09:23 +0300

    Update asus.sh

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -12,14 +12,15 @@ git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
 
-patchesdir="$outside/ksu/patches/"
-suspatchesdir="$outside/ksu/sus_patches/"
-
 echo 'CONFIG_KSU=y' >> "${defconfig_file}"
 echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
 echo 'CONFIG_KSU_MANUAL_HOOK=y' >> "${defconfig_file}"
 echo '# CONFIG_KSU_SUSFS_TRY_UMOUNT is not set' >> "${defconfig_file}"
 echo 'CONFIG_KSU_SUSFS=y' >> "${defconfig_file}"
+
+patchesdir="$outside/ksu/patches/"
+suspatchesdir="$outside/ksu/sus_patches/"
+
 patchesdir="$outside/ksu/patches/"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
