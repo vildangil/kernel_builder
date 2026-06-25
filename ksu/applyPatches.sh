c0d9bebae23604f1a5d05f9a26034349d8e9e66b
VildanG <gildanovvildan0@gmail.com>
2026-06-25 21:46:09 +0300

    Update applyPatches.sh

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -6,7 +6,7 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
+curl -LSs "https://raw.githubusercontent.com/xxblebleblexx/MultiSU/refs/heads/legacy/kernel/setup.sh" | bash -s legacy
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
